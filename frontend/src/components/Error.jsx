c71235fd1527e3122e0778e7b8153e6424c553c6
prathikshetty14 <prathikshetty1411@gmail.com>
2023-12-10 00:19:53 +0530

    Updated Error Page

--- frontend/src/components/Error.jsx
+++ frontend/src/components/Error.jsx
@@ -1,12 +1,24 @@
 import Navbar from "./Navbar";
 
-function Error(){
-    return(
-        <>
-        <Navbar />
-        <div className="container mx-auto flex items-center py-5">Error</div>
-        </>
-    )
+function Error() {
+  return (
+    <>
+      <Navbar />
+      <div className="min-h-screen flex items-center justify-center bg-gray-100">
+        <div className="bg-white p-8 rounded-md shadow-md">
+          <h2 className="text-3xl font-semibold mb-4">Oops! Something went wrong</h2>
+          <p className="text-gray-600">We apologize for the inconvenience. It seems like there's an error.</p>
+          <p className="text-gray-600">Please try again later or contact support.</p>
+          <button
+            onClick={() => window.history.back()}
+            className="mt-6 bg-blue-500 text-white p-2 rounded-md hover:bg-blue-700 focus:outline-none focus:ring focus:border-blue-300"
+          >
+            Go Back
+          </button>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Error;
