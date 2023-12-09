8df3ef4071fef00489f7578118d56d41597a2fd7
prathikshetty14 <prathikshetty1411@gmail.com>
2023-12-09 20:55:59 +0530

    updated routes for vercel

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,17 +3,7 @@ import { getBookData, getFormData } from "../controllers/api/v1/index.js";
 
 const router = Router();
 
-router.get('/api/v1/formdata', getFormData);
-router.post('/api/v1/bookdata', getBookData);
-router.get("/*", function (req,res) {
-    res.sendFile(
-        path.join(__dirname, "../client/build/index.html"),
-        function(error){
-            if(error){
-                res.status(500).send(error);
-            }
-        }
-    )
-})
+router.get('/', getFormData);
+router.post('/bookdata', getBookData);
 
 export default router;
