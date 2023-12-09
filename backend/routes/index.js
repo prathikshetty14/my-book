69be31332ecea3328349851a3a6a8cd0a74b9481
prathikshetty14 <prathikshetty1411@gmail.com>
2023-12-09 18:39:53 +0530

    heroku deployment

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,5 +5,15 @@ const router = Router();
 
 router.get('/api/v1/formdata', getFormData);
 router.post('/api/v1/bookdata', getBookData);
+router.get("/*", function (req,res) {
+    res.sendFile(
+        path.join(__dirname, "../client/build/index.html"),
+        function(error){
+            if(error){
+                res.status(500).send(error);
+            }
+        }
+    )
+})
 
 export default router;
